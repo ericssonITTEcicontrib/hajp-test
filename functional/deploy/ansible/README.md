cf646f41ee58e510d317083f79897bf85e9356a2
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2015-05-05 16:55:36 -0400

    Final changes

--- functional/deploy/ansible/README.md
+++ functional/deploy/ansible/README.md
@@ -1,5 +1,4 @@
 Ansible requires host setup with following group name assumptions hajpdemo is the main group with all VMS, AM is the activeMaster group,
 HS is the Hot Standby group and SL is the Orchestrator/Monitor.
 
-TODO Monitor launch is not automated
-TODO Ansible work is reschedule to Release 2-3
+Sample hosts.yml file is provided.
--- functional/deploy/ansible/deployViaAnsible.sh
+++ functional/deploy/ansible/deployViaAnsible.sh
@@ -3,10 +3,15 @@
 ./getBinaries.sh
 
 ansible-playbook --inventory=hosts.yml playbook-killall-java.yml
+
 ansible-playbook --inventory=hosts.yml playbook-install-jdk8.yml
+ansible-playbook --inventory=hosts.yml playbook-install-haproxy.yml
+
 ansible-playbook --inventory=hosts.yml playbook-deploy-jenkins.yml
 ansible-playbook --inventory=hosts.yml playbook-deploy-orchestratormonitor.yml
+
 ansible-playbook --inventory=hosts.yml playbook-launch-orchestrator.yml
 ansible-playbook --inventory=hosts.yml playbook-launch-jenkins.yml
+
 
 ./houseKeep.sh
